336e5c964b362d4038e49ff626c72f2e2bb4a438
Fasust <sebastianf333@googlemail.com>
2019-10-07 17:28:33 +0200

    lowering state

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,25 @@ import 'package:wisgen/blocs/favorite_bloc.dart';
 import 'package:wisgen/ui/page_favorites.dart';
 import 'package:wisgen/ui/page_wisdom_feed.dart';
 
-///As it is the Rout of the WWidget Tree, Services That need to be
-///Globally Provided are Set up here
-void main() => runApp(BlocProvider(
-      //Globally Providing the Favorite BLoC as it is needed on multiple pages
-      builder: (BuildContext context) => FavoriteBloc(),
-      child: MyApp(),
-    ));
+void main() => runApp(MyApp());
 
-///Sets Global Theme & Sets Navigation Routes. 
+///Sets Global Theme & Sets Navigation Routes.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        routes: <String, WidgetBuilder>{
-          "/favorites": (context) => FavoriteList()
-        },
-        theme: ThemeData(
-            primaryColor: Color.fromRGBO(56, 43, 115, 1),
-            accentColor: Colors.grey,
-            textTheme: TextTheme(
-                headline: TextStyle(color: Colors.white, fontSize: 23))),
-        home: WisdomFeed());
+    //Globally Providing the Favorite BLoC as it is needed on multiple pages
+    return BlocProvider(
+      builder: (BuildContext context) => FavoriteBloc(),
+      child: MaterialApp(
+          routes: <String, WidgetBuilder>{
+            "/favorites": (context) => FavoriteList()
+          },
+          theme: ThemeData(
+              primaryColor: Color.fromRGBO(56, 43, 115, 1),
+              accentColor: Colors.grey,
+              textTheme: TextTheme(
+                  headline: TextStyle(color: Colors.white, fontSize: 23))),
+          home: WisdomFeed()),
+    );
   }
 }
