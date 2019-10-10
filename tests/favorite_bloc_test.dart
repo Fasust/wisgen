ee4915d4c83c5a893a846892eae42f7508e605d8
Fasust <sebastianf333@googlemail.com>
2019-10-10 19:18:49 +0200

    test

--- tests/favorite_bloc_test.dart
+++ tests/favorite_bloc_test.dart
@@ -4,6 +4,9 @@ import 'package:wisgen/blocs/favorite_event.dart';
 import 'package:wisgen/models/wisdom.dart';
 
 void main() {
+
+  ///Related test are grouped together 
+  ///to get a more readable output 
   group('Favorite Bloc', () {
     FavoriteBloc favoriteBloc;
 
@@ -17,25 +20,25 @@ void main() {
       favoriteBloc.dispose();
     });
     
-    test('Add a Favorite and see if it is emitted by th BLoC', () {
-      Wisdom wisdom = Wisdom(id: 1, text: "Wisdom", type: "any");
+    test('Add a Favorite and see if it is emitted as state', () {
+      Wisdom wisdom = Wisdom(id: 1, text: "Back up your Pictures", type: "tech");
       favoriteBloc.dispatch(AddFavoriteEvent(wisdom));
       favoriteBloc.state.listen((data) => () {
             expect(wisdom, data);
           });
     });
 
-    test('Add and Remove a Favorite and see if the BLoC is empty', () {
-      Wisdom wisdom = Wisdom(id: 1, text: "Wisdom", type: "any");
+    test('Add and Remove a Favorite and see if the state is empty', () {
+      Wisdom wisdom = Wisdom(id: 1, text: "Back up your Pictures", type: "tech");
       favoriteBloc.dispatch(AddFavoriteEvent(wisdom));
       favoriteBloc.dispatch(RemoveFavoriteEvent(wisdom));
       expect(0, favoriteBloc.currentState.length);
     });
 
-    test('Stream many events and see if BLoC emits State in correct order', () {
-      Wisdom wisdom1 = Wisdom(id: 1, text: "Wisdom", type: "any");
-      Wisdom wisdom2 = Wisdom(id: 2, text: "Wisdom", type: "any");
-      Wisdom wisdom3 = Wisdom(id: 3, text: "Wisdom", type: "any");
+    test('Stream many events and see if the State is emitted in correct order', () {
+      Wisdom wisdom1 = Wisdom(id: 1, text: "Back up your Pictures", type: "tech");
+      Wisdom wisdom2 = Wisdom(id: 2, text: "Wash your ears", type: "Mum's Advice");
+      Wisdom wisdom3 = Wisdom(id: 3, text: "Travel while you're young", type: "Grandma's Advice");
 
       favoriteBloc.dispatch(AddFavoriteEvent(wisdom1));
       favoriteBloc.dispatch(AddFavoriteEvent(wisdom2));
@@ -45,7 +48,7 @@ void main() {
       expect(
           favoriteBloc.state,
           emitsInOrder([
-            List(),
+            List(), //BLoC Library BLoCs emit their initial State on creation
             List()..add(wisdom1),
             List()..add(wisdom1)..add(wisdom2),
             List()..add(wisdom2),
