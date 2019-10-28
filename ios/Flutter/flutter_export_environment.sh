f82f6013fef540c2316970d7316303936c3e3519
Sebastian Faust <sebastianf333@googlemail.com>
2019-10-28 13:58:38 +0100

    prep for playstore release

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Progamme\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Documents\Programmieren\wisgen"
+export "FLUTTER_ROOT=C:\Users\sebas\Documents\Programms\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sebas\Documents\Coding\wisgen"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\Progamme\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\sebas\Documents\Programms\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
